f9e36c852e5c0e3b12ab2e8bbf2b6b7a199bfe13
maxim <zanoga.maxim@gmail.com>
2022-03-23 12:11:19 +0000

    update frontend

---
+++ fontend/dist/js/main.js
@@ -0,0 +1,71 @@
+let baseURL = 'http://127.0.0.1:8000';
+let checkNameURL = baseURL + '/check/';
+let checkImageURL = baseURL + '/check/image';
+
+$('.search-button').on('click', function () {
+    clearStatus();
+    let searchValue = $(".search-input").val();
+    fetch(checkNameURL + searchValue).then(respose => {
+        return respose.json();
+    }).then(resp => {
+        checkStatus(resp.status);
+    })
+});
+
+document.getElementById("img").onchange = function () {
+    clearStatus();
+    let form = document.getElementById('ImageCheckForm');
+    let formData = new FormData(form);
+    fetch(checkImageURL, {method: 'POST', body: formData})
+        .then(response => response.json()).then(data => {
+        let count = Object.keys(data).length;
+        if (count === 1) {
+            checkStatus(Object.values(data)[0]);
+        } else if (count > 1) {
+            let tbodyRef = document.getElementById('table-status').getElementsByTagName('tbody')[0];
+            for (let i = 0; i < count; i++) {
+                let newRow = tbodyRef.insertRow();
+
+                let brand = Object.keys(data)[i];
+                let status = Object.values(data)[i];
+
+                if (status === "bloody") {
+                    newRow.className = "table-danger";
+                }
+
+                let newCell = newRow.insertCell();
+                let newText = document.createTextNode(brand);
+                newCell.appendChild(newText);
+
+                let newCell2 = newRow.insertCell();
+                let newText2 = document.createTextNode(status);
+                newCell2.appendChild(newText2);
+            }
+            $(".statusTable").show();
+        } else {
+            checkStatus("not found");
+        }
+    }).catch(error => {
+        console.log(error);
+    })
+};
+
+function checkStatus(checkStatus) {
+    if (checkStatus === "clear") {
+        $(".alert-success").show();
+    } else if (checkStatus === "pressure") {
+        $(".alert-warning").show();
+    } else if (checkStatus === "bloody") {
+        $(".alert-danger").show();
+    } else if (checkStatus === "not found") {
+        $(".alert-primary").show();
+    }
+}
+
+function clearStatus(){
+    $(".alert-warning").hide();
+    $(".alert-danger").hide();
+    $(".alert-success").hide();
+    $(".alert-primary").hide();
+    $(".statusTable").hide();
+}
